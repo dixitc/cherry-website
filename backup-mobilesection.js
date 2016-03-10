b6aef9909d8e206a7a4a5682d964cbf5c9db4a81
dixitc <dixit@triconinfotech.com>
2016-03-10 18:01:43 +0530

    cleaned up code , redoing mobile feature section

---
+++ backup-mobilesection.js
@@ -0,0 +1,87 @@
+<h2 id="it-works">How it works</h2>
+<div class="inner test-mobile">
+	<div class="feature-wrapper">
+		<div id="scene">
+			<li class="layer" data-depth="0.40">
+
+				<div class="info info-1 animated">
+
+					<div id="feat-1" class="feat" data-out-effect="fadeOut" data-in-effect="fadeIn">
+						<p class="feat-title-text">
+							Title makes a statement.
+						</p>
+						<p class="feat-sml-text">
+							Content elaborates on what title says in detail so the reader better understandards the particular details.
+						</p>
+					</div>
+					<div class="circle circle-1">
+						<div class="sml-circle"></div>
+					</div>
+					<div class="border-line border-line-1"></div>
+				</div>
+			</li>
+		</div>
+		<div id="scene-3">
+			<li class="layer" data-depth="0.40">
+				<div class="info info-3 animated">
+
+					<div id="feat-3" class="feat" data-out-effect="fadeOut" data-in-effect="fadeIn">
+						<p class="feat-title-text-left">
+							Create private albums with your friends and family
+						</p>
+						<p class="feat-sml-text-left">
+							All the photos in your memories are privately available to members only.Check suggestions for smartly grouped photos.
+						</p>
+					</div>
+					<div class="circle circle-3">
+						<div class="sml-circle-left"></div>
+					</div>
+					<div class="border-line border-line-3"></div>
+				</div>
+			</li>
+		</div>
+	</div>
+	<div class="feature-wrapper">
+		<div id="scene-2">
+			<li class="layer" data-depth="0.40">
+				<div class="info info-2 animated">
+
+					<div id="feat-2" class="feat" data-out-effect="fadeOut" data-in-effect="fadeIn">
+						<p class="feat-title-text">
+							Create private albums with your friends and family
+						</p>
+						<p class="feat-sml-text">
+							All the photos in your memories are privately available to members only.
+						</p>
+					</div>
+					<div class="circle circle-1">
+						<div class="sml-circle"></div>
+					</div>
+					<div class="border-line border-line-1"></div>
+				</div>
+			</li>
+		</div>
+		<div id="scene-4">
+			<li class="layer" data-depth="0.40">
+				<div class="info info-4 animated">
+
+					<div id="feat-3" class="feat" data-out-effect="fadeOut" data-in-effect="fadeIn">
+						<p class="feat-title-text-left">
+							Create private albums with your friends and family
+						</p>
+						<p class="feat-sml-text-left">
+							All the photos in your memories are privately available to members only.
+						</p>
+					</div>
+					<div class="circle circle-3">
+						<div class="sml-circle-left"></div>
+					</div>
+					<div class="border-line border-line-3"></div>
+				</div>
+			</li>
+		</div>
+
+
+	</div>
+
+</div>
